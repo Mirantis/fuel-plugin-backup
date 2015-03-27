7117233696d407eb49d4895c7297090aa506528c
root <root@osd1.capone.local>
2015-03-27 12:14:45 +0000

    initial commit for restore

--- project.sh
+++ project.sh
@@ -24,6 +24,9 @@ command_seq=$COMMANDS_SEQUENCE
 if [ "$1" == "backup" ]; then
   shift 1
   backup
+elif [ "$1" == "restore" ]; then
+  shift 1
+  restore
 elif [ "$1" == "purge" ]; then
   shift 1
   purge
